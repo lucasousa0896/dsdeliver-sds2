15ef6911d78bb71557d3b70a78fd400703274596
Lucas Sousa <lucas.sousa0896@gmail.com>
2021-01-08 23:34:40 -0300

    GET /products

---
+++ backend/src/main/java/com/devlucas/dsdeliver/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.devlucas.dsdeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devlucas.dsdeliver.dto.ProductDTO;
+import com.devlucas.dsdeliver.entities.Product;
+import com.devlucas.dsdeliver.repositories.ProductRepository;
+
+
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	public ProductRepository repository;
+
+	@Transactional(readOnly = true)
+	public List<ProductDTO> findAll(){
+		List<Product> list = repository.findAllByOrderByNameAsc();
+		return list.stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());
+	}
+}
